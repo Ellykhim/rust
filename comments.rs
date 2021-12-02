2625805a103bea08c3e7d75aa9c48afbe4b63e31
ellykhim <ellykhim97@gmail.com>
2021-12-02 08:43:48 +0300

    working with comments in rust

---
+++ comments.rs
@@ -0,0 +1,9 @@
+//Comments in rust language
+fn main(){
+  println!("Hello World");
+  //This is a one line comment
+  /*
+   * Multi-line comment
+   */
+}
+
--- hello.rs
+++ hello.rs
@@ -1,3 +1,4 @@
+//Hello world program in rust
 fn main(){
   println!("Hello World");
   println!("I am doing fine with rust");
